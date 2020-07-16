0203264ce2baa13d7a566c026ccea56cd68cf986
unknown <clarkalinsob101@gmail.com>
2020-07-16 19:16:26 +0800

    style signin and chatboard

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -5,36 +5,32 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
-router.post('/signup', async (req, res) => {
-  const username = req.body.username
-  const password = await bcrypt.hash(req.body.password, 10)
-
-  const newUser = new User({
-    username,
-    password
-  })
-
-  const user = await newUser.save()
-
-  res.json(user)
-})
-
 router.post('/signin', async (req, res) => {
-  console.log(req.body.username)
-  console.log(req.body.password)
   const username = req.body.username
   const password = req.body.password
 
   const user = await User.findOne({ username })
+  let token
 
-  if (!user) return res.json('No user found')
+  // if new user, create new user
+  if (!user) {
+    const hashPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({
+      username,
+      password: hashPassword
+    })
 
+    await newUser.save()
+
+    token = jwt.sign({ user: { username } }, process.env.SECRET_KEY)
+    return res.json({ token })
+  }
+
+  // if user exist, then verify
   const match = await bcrypt.compare(password, user.password)
-
   if (!match) return res.json('Password did not match')
 
-  const token = jwt.sign({ user: { username: user.username } }, process.env.SECRET_KEY)
-
+  token = jwt.sign({ user: { username } }, process.env.SECRET_KEY)
   res.json({ token })
 })
 
